ec391109d11332c6fa5c271e291f1e33813c7cb3
Maggie Morgan <mm570417@ohio.edu>
2019-01-22 11:11:53 -0500

    update git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/**************
+*	File: git-lab-program.cc
+*	Author: Maggie Morgan
+*	Date: 1/22/19
+*	Description: File for git-lab
+**************/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main (int argc, char const *argv[]) {
+	cout << "Hello Git!" << endl;
+	return 0;
+}
